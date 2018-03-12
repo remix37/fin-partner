68cf904c2d5ff585b24280be511b43872e504d98
Rémy Rigault <r.rigault@me.com>
2018-03-12 01:30:32 +0100

    design homepage

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -4,6 +4,7 @@
 //= require bootstrap-sass/assets/javascripts/bootstrap.min.js
 //= require vegas/dist/vegas.min.js
 
+// Vegas
 $(function() {
   $("#home").vegas({
       slides: [
@@ -12,4 +13,14 @@ $(function() {
           { src: "./images/slide3.jpg" }
       ]
   });
+});
+
+
+// Scroll
+$(document).ready(function() {
+	$('.js-scrollTo').on('click', function() { // Au clic sur un élément
+		var page = $(this).attr('href'); // Page cible
+    $('html, body').animate( { scrollTop: $(page).offset().top }, 750);
+		return false;
+	});
 });
