4c2667996b5ba70fde4c2cbf3ceef27cfdb976f0
Rémy Rigault <r.rigault@me.com>
2018-03-20 05:06:22 +0700

    add carousel for testimonies

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,10 +2,12 @@
 
 //= require jquery/dist/jquery.min.js
 //= require bootstrap-sass/assets/javascripts/bootstrap.min.js
-//= require vegas/dist/vegas.min.js
+//= require vegas/index.js
+//= require owl.carousel/index.js
 
-// Vegas
 $(function() {
+
+  // Vegas
   $("#home").vegas({
       slides: [
           { src: "./images/home/slide-1.jpg" },
@@ -13,14 +15,30 @@ $(function() {
           { src: "./images/home/slide-3.jpg" }
       ]
   });
-});
 
-
-// Scroll
-$(document).ready(function() {
-	$('.js-scrollTo').on('click', function() { // Au clic sur un élément
-		var page = $(this).attr('href'); // Page cible
+  // Scroll
+  $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+  	var page = $(this).attr('href'); // Page cible
     $('html, body').animate( { scrollTop: $(page).offset().top }, 750);
-		return false;
-	});
+  	return false;
+  });
+
+  // Carousel
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+  })
+
 });
